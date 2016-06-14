1909afd4079184e0ed7e3129ca6f15f685f3a8a5
Ivan D <ivan.dzh93@gmail.com>
2016-06-14 12:08:36 +0300

    added server and package

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var http = require('http');
+var app = express();
+var server = http.createServer(app); //create a server with express included
+ 
+ // serves main page
+app.use(express.static(__dirname + '/'));
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+var port = process.env.PORT || 5000;
+server.listen(port);
+console.log("Listening on port " + port);
+
