f230615e95776456349ac431fa6d2e5c0f93b714
Ivan D <ivan.dzh93@gmail.com>
2016-06-29 21:30:35 +0300

    huge upgrade, read description
    added support for languages, fixed work section, added social links, top
    menu moved down, arrow fixed, instafeed fixed

--- server.js
+++ server.js
@@ -7,9 +7,16 @@ var server = http.createServer(app); //create a server with express included
 app.use(express.static(__dirname + '/'));
 
 app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/en', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+app.get('/bg', function(req, res){
+    res.sendFile(__dirname + '/index_bg.html');
+});
 
 var port = process.env.PORT || 5000;
 server.listen(port);
